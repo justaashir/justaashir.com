6f5e9f920e1d1dbd77c8837904fbca8095836a6e
Aashir Aamir Khan <accounts@justaashir.com>
2020-08-17 00:13:55 +0500

    Uninstalled Prism

--- gridsome.config.js
+++ gridsome.config.js
@@ -65,9 +65,6 @@ module.exports = {
       externalLinksTarget: '_blank',
       externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
       anchorClassName: 'icon icon-link',
-      plugins: [
-        '@gridsome/remark-prismjs'
-      ]
     }
   }
 }
